6a77c97d795e3e2b71de753f9c11fef8cd9f9e54
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-03 15:29:02 +0100

    Python re-impl.

---
+++ 2024/03/03.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+mul_re = re.compile(r"mul\((\d+),(\d+)\)")
+dont_do_re = re.compile(r"don't\(\).*?do\(\)", flags=re.DOTALL)
+
+text = sys.stdin.read()
+
+# Part 1
+print(sum(int(m.group(1)) * int(m.group(2)) for m in mul_re.finditer(text)))
+
+# Part 2
+text = "".join(dont_do_re.split(text))
+print(sum(int(m.group(1)) * int(m.group(2)) for m in mul_re.finditer(text)))
+
