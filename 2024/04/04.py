5a017610c6252f77fd956bd563dfe112590a9377
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-04 14:01:10 +0100

    Denesting

--- 2024/04/04.py
+++ 2024/04/04.py
@@ -16,18 +16,19 @@ for row in range(ROWS):
         if matrix[row][col:col+WLEN] in TO_MATCH:
             n+=1
         # Vertical/Diagonal matches
-        if row+WLEN <= ROWS:
-            # Down to the left
-            if col+1 >= WLEN:
-                x = ''.join(matrix[row+i][col-i] for i in range(WLEN))
-                n += x in TO_MATCH
-            # Directly down
-            x = ''.join(matrix[row+i][col] for i in range(WLEN))
+        if row+WLEN > ROWS:
+            continue
+        # Down to the left
+        if col+1 >= WLEN:
+            x = ''.join(matrix[row+i][col-i] for i in range(WLEN))
             n += x in TO_MATCH
-            # Down to the right
-            if col+WLEN <= COLS:
-                x = ''.join(matrix[row+i][col+i] for i in range(WLEN))
-                n += x in TO_MATCH
+        # Directly down
+        x = ''.join(matrix[row+i][col] for i in range(WLEN))
+        n += x in TO_MATCH
+        # Down to the right
+        if col+WLEN <= COLS:
+            x = ''.join(matrix[row+i][col+i] for i in range(WLEN))
+            n += x in TO_MATCH
 print(n)
 
 # Part 2
@@ -36,16 +37,13 @@ WLEN = len(WORD)
 TO_MATCH = frozenset((WORD,WORD[::-1]))
 
 n = 0
-for row in range(ROWS):
-    for col in range(COLS):
-        # Vertical/Diagonal matches
-        if row+WLEN <= ROWS and col+WLEN <= COLS:
-            # Match down to the right?
-            x = ''.join(matrix[row+i][col+i] for i in range(WLEN))
-            if x in TO_MATCH:
-                # Matches up to the right?
-                y = ''.join(matrix[row+WLEN-1-i][col+i] for i in range(WLEN))
-                n += y in TO_MATCH
-
+for row in range(ROWS-WLEN+1):
+    for col in range(COLS-WLEN+1):
+        # Matches down to the right?
+        x = ''.join(matrix[row+i][col+i] for i in range(WLEN))
+        if x in TO_MATCH:
+            # Matches up to the right?
+            y = ''.join(matrix[row+WLEN-1-i][col+i] for i in range(WLEN))
+            n += y in TO_MATCH
 print(n)
 
