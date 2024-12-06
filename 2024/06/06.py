79f6c241305127b953d285d346fa9547967ed7a6
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-06 20:45:52 +0100

    Day 06 - part 2

--- 2024/06/06.py
+++ 2024/06/06.py
@@ -2,7 +2,6 @@ import sys
 
 IN = sys.stdin.read()
 
-OBSTACLE = "#"
 GUARD_SYMBOLS = "^>v<"  # Directions the guard can face, N,E,S,W (ORDER MATTERS!)
 
 # Directions
@@ -24,21 +23,21 @@ OBSTACLES = frozenset(
         (row, col)
         for row, line in enumerate(MAP)
         for col, cell in enumerate(line)
-        if cell == OBSTACLE
+        if cell == "#"
     ]
 )
 
 # Coordinates of where the guard starts
-curr_row, curr_col = next(
+start_pos = next(
     (row, col)
     for row, line in enumerate(MAP)
     for col, cell in enumerate(line)
     if cell in GUARD_SYMBOLS
 )
-curr_direction = MAP[curr_row][curr_col]
+row, col = start_pos
+direction = MAP[row][col]
 
 is_in_bounds = lambda row, col: 0 <= row <= MAX_ROW and 0 <= col <= MAX_COL
-is_legal_pos = lambda row, col: is_in_bounds(row, col) and (row, col) not in OBSTACLES
 turn_clockwise = lambda curr: GUARD_SYMBOLS[
     (GUARD_SYMBOLS.index(curr) + 1) % len(GUARD_SYMBOLS)
 ]
@@ -66,7 +65,41 @@ def guard_trail(row, col, direction):
 
 
 # Part 1
-print(len(set(guard_trail(curr_row, curr_col, curr_direction))))
+print(len(set(guard_trail(row, col, direction))))
 # (The set is inefficient)
 
+
 # Part 2
+def guard_trail2(row, col, direction, obstacles=OBSTACLES):
+    yield (row, col), direction
+    while True:
+        # Calculate next coordinate
+        next_row, next_col = next_pos(row, col, direction)
+        if not is_in_bounds(next_row, next_col):
+            # Guard leaves the map
+            break
+        if (next_row, next_col) in obstacles:
+            # Guard hits an obstacle and turns right/clockwise
+            direction = turn_clockwise(direction)
+            continue
+        row, col = next_row, next_col
+        yield (row, col), direction
+
+
+def goes_into_loop(iterator):
+    earlier = set()
+    for x in iterator:
+        if x in earlier:
+            return True
+        earlier.add(x)
+    return False
+
+
+spots = set()
+guard_path = list(guard_trail2(row, col, direction))
+for i, (pos, dir) in enumerate(guard_path[1:]):
+    (prev_x, prev_y), prev_dir = guard_path[i]
+    if goes_into_loop(guard_trail2(row,col,direction, OBSTACLES | {pos})):
+        spots.add(pos)
+
+print(len([x for x in spots if x != start_pos]))
