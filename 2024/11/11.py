2d1f448fea72a1318d23a8f48f11a0336976176a
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-11 15:05:49 +0100

    Day 11

---
+++ 2024/11/11.py
@@ -0,0 +1,43 @@
+import sys
+from collections import defaultdict
+
+IN = sys.stdin.read()
+NUM_BLINKS = 75
+
+stones = list(map(int, IN.split()))
+
+stones_after_blinks = defaultdict(lambda: [None for _ in range(NUM_BLINKS)])
+
+
+def one_step(n: int) -> list[str]:
+    sn = str(n)
+    l = len(sn)
+    if l % 2 == 0:
+        n1, n2 = sn[: l // 2], sn[l // 2 :]
+        return [int(n1), int(n2)]
+    if n == 0:
+        return [1]
+    return [int(n) * 2024]
+
+
+def calc_num_stones(stone: int, blinks_left: int) -> int:
+    if blinks_left <= 0:
+        return 1
+    blinks_left -= 1
+    result = stones_after_blinks[stone][blinks_left]
+    if result is not None:
+        return result
+
+    next_stones = one_step(stone)
+    num_stones = 0
+    # For each of the next stones, how many stones do we end up with after the remaining blinks
+    for s in next_stones:
+        num_stones += calc_num_stones(s, blinks_left)
+    stones_after_blinks[stone][blinks_left] = num_stones
+    return num_stones
+
+
+# Part 1
+print(sum(calc_num_stones(s, 25) for s in stones))
+# Part 2
+print(sum(calc_num_stones(s, NUM_BLINKS) for s in stones))
