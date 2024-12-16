a108b6e4d625926d9e835f2d1e891004e9e1a0c0
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-16 10:50:39 +0100

    Day 16 p1

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -2,7 +2,7 @@ import sys
 from time import sleep
 from heapq import heappush, heappop, heapify
 from collections import defaultdict
-from itertools import count,repeat,takewhile
+from itertools import count, repeat, takewhile
 from enum import IntEnum
 
 
@@ -16,7 +16,7 @@ class Dir(IntEnum):
 DIRECTIONS = (Dir.N, Dir.E, Dir.S, Dir.W)
 
 
-def forward(r):
+def beam_forward(r, walls):
     x, y, d = r
     if d == Dir.N:
         return x - 1, y, d
@@ -77,20 +77,50 @@ def parse_input(grid):
     return reindeer, target, frozenset(walls), frozenset(nonwalls)
 
 
-def next_states(cost, reindeer, walls):
-    fx, fy, fd = forward(reindeer)
-    if (fx, fy) not in walls:
-        yield cost + FORWARD_COST, (fx, fy, fd)
-    yield cost + TURN_COST, turn_cw(reindeer)
-    yield cost + TURN_COST, turn_ccw(reindeer)
+def get_junctions(reindeer, target, walls, nonwalls):
+    junctions = {reindeer[:2], target}
+    for cell in nonwalls:
+        for beam in beams(cell, walls):
+            if beam:
+                # Add last cell in every beam as a junction
+                junctions.add(beam[-1])
+    return junctions
+
+def manhattan_dist(c1,c2):
+    return abs(c1[0]-c2[0]) + abs(c1[1]-c2[1])
+
+def next_states(cost, reindeer, junctions, walls):
+    # Reachable junctions
+    x,y,d = reindeer
+    for i,beam in enumerate(beams((x,y), walls)):
+        bd = Dir(i)
+        # for bx,by in filter(lambda c: c in junctions, beam):
+        for bx,by in beam:
+            inc = FORWARD_COST * manhattan_dist((x,y), (bx,by))
+            if bd != d:
+                inc += TURN_COST
+            yield cost+inc, (bx,by, bd)
+        
+    # d = reindeer[-1]
+    # beam = beams(reindeer[:2], walls)[d.value]
+    # for i,cell in enumerate(beam,start=1):
+    #     # if cell in junctions:
+    #     yield cost + FORWARD_COST*i, (*cell, d)
+    # yield cost + TURN_COST, turn_cw(reindeer)
+    # yield cost + TURN_COST, turn_ccw(reindeer)
 
 
-def reconstruct_path(came_from, current):
-    total_path = [current]
-    while current in came_from:
-        current = came_from[current]
-        total_path.append(current)
-    return total_path[::-1]
+def reconstruct_paths(came_from, current):
+    total_path = set()
+    to_expand = {current}
+    while to_expand:
+        current = to_expand.pop()
+        for predecessor in came_from[current]:
+            x,y,d = predecessor
+            if (x,y) not in total_path:
+                to_expand.add(predecessor)
+            total_path.add((x,y))
+    return total_path
 
 
 H_SCORE = defaultdict(lambda: float("inf"))
@@ -98,28 +128,35 @@ H_SCORE = defaultdict(lambda: float("inf"))
 
 def h(state, target):
     """Heuristic function"""
-    cost, reindeer = state
-    rx, ry, d = reindeer
-    return H_SCORE[(rx, ry)]
-    # # Manhattan distance to goal
     # cost, reindeer = state
     # rx, ry, d = reindeer
-    # tx, ty = target
-    # return abs(rx - tx) + abs(ry - ty)
+    # return H_SCORE[(rx, ry)]
+    # Manhattan distance to goal
+    reindeer = state
+    rx, ry, d = reindeer
+    tx, ty = target
+    return abs(rx - tx) + abs(ry - ty)
 
 
 def beams(cell, walls):
     x, y = cell
-    # Beam south
-    s_beam = list(takewhile(lambda c: c not in walls, zip(count(start=x+1,step=1), repeat(y))))
-    # Beam west
-    w_beam = list(takewhile(lambda c: c not in walls, zip(repeat(x), count(start=y-1,step=-1))))
     # Beam north
-    n_beam = list(takewhile(lambda c: c not in walls, zip(count(start=x-1,step=-1), repeat(y))))
+    n_beam = list(
+        takewhile(lambda c: c not in walls, zip(count(start=x - 1, step=-1), repeat(y)))
+    )
     # Beam east
-    e_beam = list(takewhile(lambda c: c not in walls, zip(repeat(x), count(start=y+1,step=1))))
-    return (s_beam,w_beam,n_beam,e_beam)
-
+    e_beam = list(
+        takewhile(lambda c: c not in walls, zip(repeat(x), count(start=y + 1, step=1)))
+    )
+    # Beam south
+    s_beam = list(
+        takewhile(lambda c: c not in walls, zip(count(start=x + 1, step=1), repeat(y)))
+    )
+    # Beam west
+    w_beam = list(
+        takewhile(lambda c: c not in walls, zip(repeat(x), count(start=y - 1, step=-1)))
+    )
+    return (s_beam, w_beam, n_beam, e_beam)
 
 
 def calc_h_score(target, walls, nonwalls):
@@ -131,56 +168,66 @@ def calc_h_score(target, walls, nonwalls):
     while beam_centers:
         curr = beam_centers.pop()
         score = H_SCORE[curr] + turn_cost
-        for beam in beams(curr,walls):
+        for beam in beams(curr, walls):
             if not beam:
                 continue
             beam_end = beam[-1]
             beam_end_old_score = H_SCORE[beam_end]
-            for inc,cell in enumerate(beam,start=1):
+            for inc, cell in enumerate(beam, start=1):
                 H_SCORE[cell] = min(H_SCORE[cell], score + inc)
             if beam_end_old_score != H_SCORE[cell]:
                 # We updated the score at the beam end, rerun from beam_end
                 beam_centers.add(beam_end)
-        turn_cost = TURN_COST # All beams which arent the first one need turn cost
+        turn_cost = TURN_COST  # All beams which arent the first one need turn cost
     # print(H_SCORE)
     print("Done precomputing H score")
 
 
-def a_star(start, target, walls, nonwalls):
+def a_star(x,y,d, target, junctions, walls, nonwalls):
     print("Starting A* ...")
-    openheap = [start]
-    came_from = {}
+    came_from = defaultdict(list)
     g_score = defaultdict(lambda: float("inf"))
-    g_score[start] = 0
+    g_score[(x,y)] = 0
     f_score = defaultdict(lambda: float("inf"))
-    f_score[start] = h(start, target)
+    fsc = h((x,y,d), target)
+    f_score[(x,y)] = fsc
+    openheap = [(fsc,x,y,d)]
 
-    closest = float('inf')
+    closest = float("inf")
     while openheap:
-        current = heappop(openheap)
-        closest = min(closest,abs(current[1][0]-target[0])+abs(current[1][1]-target[1]))
-        print(closest, end='\r')
-        ccost, creindeer = current
-        if creindeer[0] == target[0] and creindeer[1] == target[1]:
-            return ccost, reconstruct_path(came_from, current)
-        for neighbor in next_states(*current, walls):
-            tentative_g_score = neighbor[0]
-            if tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = tentative_g_score + h(neighbor, target)
-                if neighbor not in openheap:
-                    heappush(openheap, neighbor)
-    raise ValueError("Nothing found!")
+        fsc, x,y,d = heappop(openheap)
+        # print(x,y,d)
+        closest = min(closest, manhattan_dist((x,y), target))
+        print(closest,len(openheap), "remaining", end="\r")
+        ccost = g_score[(x,y)]
+        # if (x,y) == target:
+        #     return ccost, reconstruct_paths(came_from, (x,y,d))
+        for ncost, (nx,ny,nd) in next_states(ccost, (x,y,d), junctions, walls):
+            tentative_g_score = ncost
+            if tentative_g_score == g_score[(nx,ny)]:
+                came_from[(nx,ny,nd)].append((x,y,d))
+            if tentative_g_score < g_score[(nx,ny)]:
+                came_from[(nx,ny,nd)] = [(x,y,d)]
+                g_score[(nx,ny)] = tentative_g_score
+                nfsc = tentative_g_score + h((nx,ny,nd), target)
+                f_score[(nx,ny)] = nfsc
+                neighb = (nfsc, nx,ny,nd)
+                if neighb not in openheap:
+                    heappush(openheap, neighb)
+
+    return ccost,reconstruct_paths(came_from, target)
 
 
 def main():
     reindeer, target, walls, nonwalls = parse_input(get_input())
+    junctions = get_junctions(reindeer, target, walls, nonwalls)
     # Precompute heuristic score
     calc_h_score(target, walls, nonwalls)
-    path_cost, path = a_star((0, reindeer), target, walls, nonwalls)
+    path_cost, path = a_star(*reindeer, target, junctions, walls, nonwalls)
 
+    print()
     print(path_cost)
+    print(len(path))
 
 
 main()
