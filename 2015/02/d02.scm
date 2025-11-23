2453322224a4e96ea0fb19b35260bda08311b772
Logi E. <logi.eyjolfsson@gmail.com>
2025-11-23 23:39:54 +0000

    Refactor 2015-03

--- 2015/02/d02.scm
+++ 2015/02/d02.scm
@@ -5,40 +5,40 @@
 (use-modules (ice-9 match))
 
 (define (get-input)
-  "Reads all text from STDIN or from a file if a filename was given as command-line argument."
-  (let ((args (cdr (program-arguments))))
-    (string-trim-both (if (null? args)
-                          (get-string-all (current-input-port))
-                          (call-with-input-file (car args) get-string-all)))))
+  "Reads all text from './in' or from different file if specified as CLI argument."
+  (string-trim-both
+   (call-with-input-file
+       (if (null? (cdr (program-arguments))) "./in" (car args))
+     get-string-all)))
 
 (define input (get-input))
 (define input-lines (string-split input #\newline))
+(define dimensions (map (lambda (line) (map string->number (string-split line #\x))) input-lines))
 
-(define (dimensions line) (map string->number (string-split line #\x)))
 (define (side-areas l w h) (list (* l w) (* w h) (* h l)))
 (define (side-circumferences l w h) (list (+ l w l w) (+ w h w h) (+ l h l h)))
 (define (volume l w h) (* l w h))
 
 (define (line->wrap line)
   "Convert line in input to feet of wrapping paper."
-  (match-let* (((l w h) (dimensions line))
+  (match-let* (((l w h) line)
                ((sa sb sc) (side-areas l w h)))
     (+ (* 2 sa) (* 2 sb) (* 2 sc) (min sa sb sc))))
 
 (define (line->ribbon line)
   "Convert line to feet of ribbon."
-  (match-let* (((l w h) (dimensions line))
+  (match-let* (((l w h) line)
                ((pa pb pc) (side-circumferences l w h)))
     (+ (volume l w h) (min pa pb pc))))
 
-(define (part1 input)
-  "Solves part 1."
-  (fold + 0 (map line->wrap input)))
+(define part1
+  (fold + 0 (map line->wrap dimensions)))
 
-(define (part2 input)
-  "Solves part 2."
-  (fold + 0 (map line->ribbon input)))
+(define part2
+  (fold + 0 (map line->ribbon dimensions)))
 
-(display (part1 input-lines)) (newline)
-(display (part2 input-lines)) (newline)
 
+(display part1) (newline)
+(display part2) (newline)
+
+(cons part1 part2)
